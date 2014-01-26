40c6de5b1768453265e2e3a5602755bc496ed252
a0091847@nus.edu.sg <a0091847@nus.edu.sg@30e6e089-44bb-5a38-4a1b-e2046e140c64>
2014-01-26 02:35:00 +0000

    Added CDToolTest class

---
+++ Shell/test/sg/edu/nus/comp/cs4218/impl/fileutils/CDToolTest.java
@@ -0,0 +1,34 @@
+package sg.edu.nus.comp.cs4218.impl.fileutils;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CDToolTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testExecute() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCDTool() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testChangeDirectory() {
+		fail("Not yet implemented");
+	}
+
+}
