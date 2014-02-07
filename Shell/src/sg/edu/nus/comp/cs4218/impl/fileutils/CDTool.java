092da8acb2b0369106a8ddcf57d150d5231c8be7
a0091847@nus.edu.sg <a0091847@nus.edu.sg@30e6e089-44bb-5a38-4a1b-e2046e140c64>
2014-02-07 14:52:12 +0000

    cdtool changed to adapt the new path util

--- Shell/src/sg/edu/nus/comp/cs4218/impl/fileutils/CDTool.java
+++ Shell/src/sg/edu/nus/comp/cs4218/impl/fileutils/CDTool.java
@@ -51,26 +51,21 @@ public class CDTool extends ATool implements ICdTool {
 		// if no error occurs, the first argument should be the path we want
 		// to change to
 		
-		//TODO: confirm with Zhuochun if argList.getParam(0) return the first
-		// param!!!!
-		String newDir = PathUtils.PathResolver(workingDir,
-											   argList.getParam(0));
+		File newDir = new File (PathUtils.PathResolver(workingDir,
+											   argList.getParam(0)));
 		
 		// if the new directory is null, the input file does not exist
-		if (newDir == null) {
-			//TODO: now the status code is some random number, confirm again!
+		if (!newDir.exists()) {
 			setStatusCode(9);
 			output = "No such file or directory!";
 		} else {
 			// now we should check if the path is essentially a file
-			if (new File(newDir).isFile()) {
-				//TODO: now the status code is some random number, confirm
-				//again!
+			if (newDir.isFile()) {
 				setStatusCode(9);
 				output = PathUtils.GetLastElementOfPath(newDir) + "is a file!";
 			} else {
 				// everything is fine, we can change shell's default directory
-				workingDir = changeDirectory(newDir);
+				workingDir = changeDirectory(newDir.toString());
 				// in this case, user will see the new directory in the shell
 				// when he executes the next command, so no explicit output is
 				// needed
