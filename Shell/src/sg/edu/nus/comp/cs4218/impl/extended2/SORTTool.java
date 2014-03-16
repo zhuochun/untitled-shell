695f268d7e04391e0d2dcbc6b0f1b363d478ff6e
westernwolf103@gmail.com <westernwolf103@gmail.com@30e6e089-44bb-5a38-4a1b-e2046e140c64>
2014-03-16 14:33:31 +0000

    refactor

--- Shell/src/sg/edu/nus/comp/cs4218/impl/extended2/SORTTool.java
+++ Shell/src/sg/edu/nus/comp/cs4218/impl/extended2/SORTTool.java
@@ -52,32 +52,6 @@ public class SORTTool extends ATool implements ISortTool {
 					sortedString = sortedString.append(stringToSortVect.get(i)+"\n");
 				}
 				return new String(sortedString);
-
-
-//		try{
-//			File f = new File(input);
-//			FileReader fr = new FileReader(f);
-//			BufferedReader br = new BufferedReader(fr);
-//			String readin = new String();
-//			String tmp = br.readLine();
-//			while(tmp != null){
-//				System.out.println(tmp);
-//				readin += tmp;
-//			}
-//
-//			StringBuilder sortedString = new StringBuilder();
-//			String []stringToSort = readin.split("\r\n");
-//			Vector <String> stringToSortVect = new Vector<String>(Arrays.asList(stringToSort));
-//			Collections.sort(stringToSortVect);
-//			for(int i = 0 ; i < stringToSortVect.size();i++){
-//				sortedString = sortedString.append(stringToSortVect.get(i)+"\n");
-//			}
-//
-//			return new String(sortedString);
-//		}catch(Exception e){
-//			e.printStackTrace();
-//		}
-//		return null;
 	}
 
 	@Override
@@ -86,7 +60,6 @@ public class SORTTool extends ATool implements ISortTool {
 		int disorderLineIndex = -1;  
 		String []stringToCheck = input.split("\r\n");
 		Vector <String> stringToCheckVect = new Vector<String>(Arrays.asList(stringToCheck));
-		//System.out.println(stringToCheckVect.size());
 		System.out.println(stringToCheckVect.get(0));
 
 		for(int i = 0;i < stringToCheckVect.size()-1; i++){
@@ -106,47 +79,6 @@ public class SORTTool extends ATool implements ISortTool {
 		else{
 			return "";
 		}
-
-
-//		try{
-//			File f = new File(input);
-//			FileReader fr = new FileReader(f);
-//			BufferedReader br = new BufferedReader(fr);
-//			String readin = new String();
-//			String tmp = br.readLine();
-//			while(tmp != null){
-//				readin += tmp;
-//			}
-//
-//			StringBuilder disorderInfo = new StringBuilder();
-//			int disorderLineIndex = -1;  
-//			String []stringToCheck = readin.split("\r\n");
-//			Vector <String> stringToCheckVect = new Vector<String>(Arrays.asList(stringToCheck));
-//			//System.out.println(stringToCheckVect.size());
-//			System.out.println(stringToCheckVect.get(0));
-//
-//			for(int i = 0;i < stringToCheckVect.size()-1; i++){
-//				if(stringToCheckVect.get(i).compareToIgnoreCase(stringToCheckVect.get(i+1))>0 && 
-//						disorderLineIndex < 0){
-//					disorderLineIndex = i+1;
-//				}
-//			}
-//
-//			if(disorderLineIndex >= 0){
-//				disorderInfo.append("sort: sortFile.txt:");
-//				disorderInfo.append(""+(disorderLineIndex+1)+" ");
-//				disorderInfo.append("disorder: ");
-//				disorderInfo.append(""+stringToCheckVect.get(disorderLineIndex)+"\n");
-//				return new String(disorderInfo);
-//			}
-//			else{
-//				return "";
-//			}
-//		}catch(Exception e){
-//			e.printStackTrace();
-//		}
-//		return null;
-
 	}
 
 	@Override
--- Shell/test/sg/edu/nus/comp/cs4218/impl/extended2/SORTToolTest.java
+++ Shell/test/sg/edu/nus/comp/cs4218/impl/extended2/SORTToolTest.java
@@ -8,9 +8,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import sg.edu.nus.comp.cs4218.extended2.ISortTool;
@@ -21,22 +19,7 @@ public class SORTToolTest {
 	private ISortTool sortTool;
 	File myFile1;
 	File myFile2;
-	
-//	@BeforeClass
-//	public void executeThisBeforeClass() throws IOException {
-//
-//		// creating testFile of unsorted order
-//		myFile1 = new File("unSortFile.txt");
-//		myFile1.createNewFile();
-//		writeFile("unSortFile.txt", "zzz\r\nbbb\r\naaa\r\nggg\r\nfff");
-//
-//		// creating testFile of sorted order
-//		myFile2 = new File("sortFile.txt");
-//		myFile2.createNewFile();
-//		writeFile("sortFile.txt", "aaa\r\nbbb\r\nccc\r\nddd\r\neee");
-//
-//	}
-	
+
 	public static void writeFile(String fileName, String s) throws IOException {
 		BufferedWriter out = new BufferedWriter(new FileWriter(fileName));
 		out.write(s);
@@ -71,22 +54,6 @@ public class SORTToolTest {
 		}
 	}
 
-//	@AfterClass
-//	public static void executeThisAfterClass() {
-//
-//		File file1 = new File("sortFile.txt");
-//		if (file1.exists()) {
-//			file1.delete();
-//		}
-//
-//		File file2 = new File("unSortFile.txt");
-//		if (file2.exists()) {
-//			file2.delete();
-//		}
-//	}
-	
-	//test sortFile method on sorted file
-	// input should be string 
 	@Test
 	public void sortFileTestForSortedFile() {
 		String result = sortTool.sortFile("aaa\r\nbbb\r\nccc\r\nddd\r\neee");
@@ -96,41 +63,31 @@ public class SORTToolTest {
 	//test sortFile method on unsorted file
 	@Test
 	public void sortFileTestForUnsortedFile() {
-		//executeThisBeforeClass
 		String result = sortTool.sortFile("zzz\r\nbbb\r\naaa\r\nggg\r\nfff");
-		//String result = sortTool.sortFile("unSortFile.txt");
 		assertEquals(result, "aaa\nbbb\nfff\nggg\nzzz\n");
-		//executeThisAfterClass
 	}
 
 	//test checkIfSorted method on sorted file
 	@Test
 	public void checkIfSortedTestForUnsortedFile() {// the input is file, function input in a string recheck
-		//executeThisBeforeClass
 		String result = sortTool.checkIfSorted("zzz\r\nbbb\r\naaa\r\nggg\r\nfff");
-		//String result = sortTool.checkIfSorted("unSortFile.txt");
 		assertEquals(result, "sort: sortFile.txt:2 disorder: bbb\n");
-		//executeThisAfterClass
 	}
 
 	//test checkIfSorted method on unsorted file
 	@Test
 	public void checkIfSortedTestForSortedFile() {
-		//executeThisBeforeClass
 		String result = sortTool.checkIfSorted("aaa\r\nbbb\r\nccc\r\nddd\r\neee");
-		//String result = sortTool.checkIfSorted("sortFile.txt");
 		assertEquals(result, "");
-		//executeThisAfterClass
 	}
 
 	@Test
 	public void executeWithIllegalOption(){
-		
 		ISortTool newSortTool = new SORTTool(new String[]{"-i"});
 		newSortTool.execute(null, null);
 		assertEquals(9,newSortTool.getStatusCode());
 	}
-	
+
 	@Test
 	public void executeWithNoOption(){
 		ISortTool newSortTool = new SORTTool(new String[]{"sortFile.txt"});
@@ -138,7 +95,7 @@ public class SORTToolTest {
 		System.out.println(result);
 		assertEquals(result, newSortTool.sortFile("aaa\r\nbbb\r\nccc\r\nddd\r\neee"));
 
-		
+
 	}
 	@Test
 	public void executeWithCOption(){
@@ -148,13 +105,13 @@ public class SORTToolTest {
 		assertEquals(result,newSortTool.checkIfSorted("aaa\r\nbbb\r\nccc\r\nddd\r\neee"));
 
 	}
-	
+
 	@Test
 	public void executeWithHelpOption(){
 		ISortTool newSortTool = new SORTTool(new String[]{"-help"});
 		String result = newSortTool.execute(null, null);
 		assertEquals(result,newSortTool.getHelp());
 	}
-	
+
 
 }
