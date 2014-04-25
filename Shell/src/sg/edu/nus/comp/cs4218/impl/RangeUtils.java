08828289efc0f5d1181c194f77dc23c32d691fd9
a0091847@nus.edu.sg <a0091847@nus.edu.sg@30e6e089-44bb-5a38-4a1b-e2046e140c64>
2014-04-25 17:23:17 +0000

    RangeUtils comments added.

--- Shell/src/sg/edu/nus/comp/cs4218/impl/RangeUtils.java
+++ Shell/src/sg/edu/nus/comp/cs4218/impl/RangeUtils.java
@@ -5,14 +5,33 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
+/**
+ * This class deals with Range issues.
+ */
 public class RangeUtils {
+	/**
+	 * This class defines a range with two parameters: left and right. The
+	 * resulted range is [left, right], inclusive.
+	 */
 	public class Range{
 		public int left, right;
 		
+		/**
+		 * Default constructor. Initialize the range to be [0, 0].
+		 */
 		public Range() {
 			left = right = 0;
 		}
 		
+		/**
+		 * Alternative constructor. Initialize the range to be [a, b], where
+		 * a and b are parameters specified by the caller.
+		 * 
+		 * @param a
+		 * 		is the lower bound of the range (inclusive).
+		 * @param b
+		 * 		is the upper bound of the range (inclusive). 
+		 */
 		public Range(int a, int b) {
 			left = a;
 			right = b;
@@ -22,22 +41,33 @@ public class RangeUtils {
 			}
 		}
 		
-		public Range(Range a) {
-			left = a.left;
-			right = a.right;
+		/**
+		 * Alternative constructor. Initialize the range to be A, where A
+		 * should be specified by the caller.
+		 *  
+		 * @param A
+		 * 		is the expected range.
+		 */
+		public Range(Range A) {
+			left = A.left;
+			right = A.right;
 		}
 	};
 	
 	/**
-	 * Parse a list of range from String to a list of Range. The String must be
-	 * in a format of "a-b,c-d,e-f", where a, b, c, d, e, f are positive
-	 * integers and left hand side of '-' must less or equal to right hand side
-	 * of '-'.
+	 * This function is used to parse a list of range from String to a list of
+	 * Range.
+	 * 
+	 * @param list
+	 * 		is the String specifies a list of the range. The String must be in
+	 * a format of "a-b,c-d,e-f", where a, b, c, d, e, f are positive integers
+	 * and left hand side of '-' must less or equal to right hand side of '-'.
 	 * 
 	 * Note: String parsed to here won't contain any spaces since all the
 	 * spaces will be eliminated during constructing the ArgList.
-	 * @param list
+	 * 
 	 * @return
+	 * 		an ArrayList of unresolved Range.
 	 */
 	public static ArrayList<Range> parseRange(String list) {
 		ArrayList<Range> rangeList = new ArrayList<Range>();
@@ -76,11 +106,16 @@ public class RangeUtils {
 	}
 	
 	/**
-	 * Merge two ranges if two ranges has non-zero intersection.
+	 * This function is used to merge two ranges.
 	 * 
 	 * @param a
+	 * 		one of the range under merge.
 	 * @param b
+	 * 		the other range under merge.
 	 * @return
+	 * 		an ArrayList of the resolved range. It contains 2 ranges if the
+	 * two given ranges cannot be merged; it contains 1 merged range if the
+	 * two given ranges has an intersection.
 	 */
 	public static ArrayList<Range> mergeRange(Range a, Range b) {
 		ArrayList<Range> range = new ArrayList<Range>();
@@ -110,11 +145,16 @@ public class RangeUtils {
 	}
 	
 	/**
-	 * Sort the range list according to left end. If two ranges share the same
-	 * left end, one with smaller right end will be prioritized.
+	 * This function is used to merge and sort the range list according to
+	 * left end.
+	 * 
+	 * If two ranges share the same left end, one with smaller right end will
+	 * be prioritized.
 	 *   
 	 * @param rangeList
+	 * 		is a list of unresolved Range.
 	 * @return
+	 * 		an ArrayList of resolved and sorted Range.
 	 */
 	public static ArrayList<Range> mergeRange(ArrayList<Range> rangeList) {
 		ArrayList<Range> finalRange = new ArrayList<Range>();
