dcacd86b4529ba1bc5c24108bff99fd321bf5077
a0091847@nus.edu.sg <a0091847@nus.edu.sg@30e6e089-44bb-5a38-4a1b-e2046e140c64>
2014-04-25 17:09:23 +0000

    PathUtils commets added.

--- Shell/src/sg/edu/nus/comp/cs4218/impl/PathUtils.java
+++ Shell/src/sg/edu/nus/comp/cs4218/impl/PathUtils.java
@@ -10,7 +10,9 @@ import java.nio.file.Paths;
 public class PathUtils {
 
 	/**
-	 * This functions takes in the current working path and the new path, and
+	 * This function is used to resolve the path information.
+	 * 
+	 * The function takes in the current working path and the new path, and
 	 * will return the resolved path. If the path represents a possible 
 	 * directory, the returned path will have a '/' at behind. Any duplicate
 	 * '/' will be resolved to only one '/'. E.g. "adsf///asdf//sd/s/" will be
@@ -24,9 +26,11 @@ public class PathUtils {
 	 * 		2. Starting with '/' will be considered as starting from root dir
 	 * 		3. All '*' fuzzy search expression are not supported currently  
 	 * 
-	 * @param currentPath
-	 * @param newPath
+	 * Please be noted that this function will not verify if the path is valid,
+	 * the caller should validate the path by itself.
+	 * 
 	 * @return
+	 * 		the resolved absolute path.
 	 */
 	public static String pathResolver(String currentPath, String newPath) {
 		String finalAbsolutePath;
@@ -61,18 +65,38 @@ public class PathUtils {
 		return finalAbsolutePath;
 	}
 	
+	// Alternative interface of pathResover
 	public static String pathResolver(File currentPath, String newPath) {
 		return pathResolver(currentPath.toString(), newPath);
 	}
 
+	/**
+	 * This function is used to get the last element on a file path. The last
+	 * element is the last file/ directory on the path.
+	 * 
+	 * @param path
+	 * 		is the path under query.
+	 * @return
+	 * 		the last element of the given path.
+	 */
 	public static String getLastElementOfPath(String path) {
 		return Paths.get(path).getFileName().toString();
 	}
 	
+	// Alternative interface of getLastElementOfPath
 	public static String getLastElementOfPath(File path) {
 		return getLastElementOfPath(path.toString());
 	}
 	
+	/**
+	 * This function is used to get a random sub-path from a given path. The
+	 * resulted sub-path's depth will be longer than 2.
+	 * 
+	 * @param path
+	 * 		is the original path.
+	 * @return
+	 * 		a random sub-path generated from the given path.
+	 */
 	public static Path getRandomSubpath(Path path) {
 		int depth = path.getNameCount();
 		int newDepth;
@@ -87,6 +111,12 @@ public class PathUtils {
 		return Paths.get("/" + path.subpath(0, newDepth).toString());
 	}
 	
+	/**
+	 * This function is used to get the current absolute working path.
+	 * 
+	 * @return
+	 * 		the current absolute working path.
+	 */
 	public static Path getCurrentPath() {
 		return Paths.get(System.getProperty("user.dir") + "/");
 	}
